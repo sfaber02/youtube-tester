af0095011f99769b509c2bb74d05c4dd5030c9cf
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-05-08 16:21:54 -0400

    fixed comments

--- src/app.js
+++ src/app.js
@@ -14,59 +14,59 @@ const App = () => {
 
     useEffect(() => {
         if (starter) {
-
             const ctx = new (window.AudioContext || window.webkitAudioContext)();
             const audioElement = document.querySelector('audio');
             console.log(audioElement);
             const mediaElement = ctx.createMediaElementSource(audioElement);
 
-            const filter = ctx.createBiquadFilter();
-            filter.frequency.value = 100;
-
-            const compressor = ctx.createDynamicsCompressor();
-            compressor.ratio.value = 5;
-            compressor.threshold.value = -50;
-
-            const delay = ctx.createDelay();
-            delay.delayTime.value = .5;
-
-
-            mediaElement.connect(delay);
-            delay.connect(filter);
-            filter.connect(compressor);
-
-            compressor.connect(ctx.destination);
-
-
-            // DELAY
-            // const delayNode = ctx.createDelay(100)
-            // const feedbackNode = ctx.createGain();
-            // const bypassNode = ctx.createGain();
-            // const masterNode = ctx.createGain();
+            //EFFECT CHAIN 1
             // const filter = ctx.createBiquadFilter();
-            // const filter2 = ctx.createBiquadFilter()
+            // filter.frequency.value = 100;
 
-            // // .02 .05 1  = slapback delay 
-            // delayNode.delayTime.value = .02;
-            // feedbackNode.gain.value = .05;
-            // bypassNode.gain.value = 1;
-            // filter.frequency.value = 6000;
-            // filter2.frequency.value = 6000;
+            // const compressor = ctx.createDynamicsCompressor();
+            // compressor.ratio.value = 5;
+            // compressor.threshold.value = -50;
 
-            // //wire up nodes
-            // mediaElement.connect(delayNode);
-            // delayNode.connect(feedbackNode);
-            // feedbackNode.connect(delayNode);
+            // const delay = ctx.createDelay();
+            // delay.delayTime.value = .5;
 
-            // delayNode.connect(bypassNode);
-            // bypassNode.connect(filter);
-            // filter.connect(masterNode);
-            // mediaElement.connect(filter2);
-            // filter2.connect(masterNode);
 
-            // masterNode.connect(ctx.destination);
+            // mediaElement.connect(delay);
+            // delay.connect(filter);
+            // filter.connect(compressor);
 
-            // mediaElement.connect(ctx.destination);
+            // compressor.connect(ctx.destination);
+
+
+            // EFFECT CHAIN 2
+            const delayNode = ctx.createDelay(100)
+            const feedbackNode = ctx.createGain();
+            const bypassNode = ctx.createGain();
+            const masterNode = ctx.createGain();
+            const filter = ctx.createBiquadFilter();
+            const filter2 = ctx.createBiquadFilter()
+
+            // .02 .05 1  = slapback delay 
+            delayNode.delayTime.value = .02;
+            feedbackNode.gain.value = .05;
+            bypassNode.gain.value = 1;
+            filter.frequency.value = 6000;
+            filter2.frequency.value = 6000;
+
+            //wire up nodes
+            mediaElement.connect(delayNode);
+            delayNode.connect(feedbackNode);
+            feedbackNode.connect(delayNode);
+
+            delayNode.connect(bypassNode);
+            bypassNode.connect(filter);
+            filter.connect(masterNode);
+            mediaElement.connect(filter2);
+            filter2.connect(masterNode);
+
+            masterNode.connect(ctx.destination);
+
+            mediaElement.connect(ctx.destination);
 
         }
 
@@ -86,7 +86,7 @@ const App = () => {
 
     return (
         <>
-            <audio src={song1} controls></audio>
+            <audio src={song2} controls></audio>
             <button onClick={start}>turn effects on</button>
             
         </>
