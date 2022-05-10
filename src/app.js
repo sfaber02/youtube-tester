ea064b7a36f31c601e67d6c1c73c5baac7ef5d82
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-05-09 20:54:18 -0400

    commit for test deploy

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import YouTube from 'react-youtube';
 
 import './style.css';
@@ -16,8 +16,12 @@ const App = () => {
         if (starter) {
             const ctx = new (window.AudioContext || window.webkitAudioContext)();
             const audioElement = document.querySelector('audio');
-            console.log(audioElement);
             const mediaElement = ctx.createMediaElementSource(audioElement);
+            const canvas = document.getElementById('canvas');
+            console.log (canvas);
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            const canvasCtx = canvas.getContext('2d');
 
             //EFFECT CHAIN 1
             // const filter = ctx.createBiquadFilter();
@@ -44,51 +48,106 @@ const App = () => {
             const bypassNode = ctx.createGain();
             const masterNode = ctx.createGain();
             const filter = ctx.createBiquadFilter();
-            const filter2 = ctx.createBiquadFilter()
+            const filter2 = ctx.createBiquadFilter();
+            const analyser = ctx.createAnalyser();
 
             // .02 .05 1  = slapback delay 
             delayNode.delayTime.value = .02;
             feedbackNode.gain.value = .05;
             bypassNode.gain.value = 1;
-            filter.frequency.value = 6000;
-            filter2.frequency.value = 6000;
+            filter.frequency.value = 20000;
+            filter2.frequency.value = 20000;
 
+            
             //wire up nodes
-            mediaElement.connect(delayNode);
+            mediaElement.connect(analyser);
+            analyser.connect(delayNode);
             delayNode.connect(feedbackNode);
             feedbackNode.connect(delayNode);
-
+            
             delayNode.connect(bypassNode);
             bypassNode.connect(filter);
             filter.connect(masterNode);
             mediaElement.connect(filter2);
             filter2.connect(masterNode);
-
+            
             masterNode.connect(ctx.destination);
-
+            
             mediaElement.connect(ctx.destination);
+            
+            //set up analyser
+            console.log (canvas.height, canvas.width);
+            analyser.fftSize = 16384;
+            let bufferLength = analyser.frequencyBinCount;
+            let dataArray = new Uint8Array(bufferLength);
+            const barWidth = (canvas.width / bufferLength) * 50; 
+            let barHeight;
+            let x = 0;
+            console.log (canvasCtx);
 
+            // render visualizer
+            const renderFrame = () => {
+                requestAnimationFrame(renderFrame);
+                x = 0;
+                // console.log (analyser);
+        
+                analyser.getByteFrequencyData(dataArray);
+                
+                canvasCtx.fillStyle = "rgba(0,0,0,0.2)"; // Clears canvas before rendering bars (black with opacity 0.2)
+                canvasCtx.fillRect(0, 0, canvas.width, canvas.height); // Fade effect, set opacity to 1 for sharper rendering of bars
+        
+                let r, g, b;
+                let bars = 150;
+        
+                for (let i = 0; i < bars; i++) {
+                    barHeight = (dataArray[i] * 2);
+                    // console.log (dataArray[i]);
+                    if (dataArray[i] > 210){ // pink
+                      r = 250
+                      g = 0
+                      b = 255
+                    } else if (dataArray[i] > 200){ // yellow
+                      r = 250
+                      g = 255
+                      b = 0
+                    } else if (dataArray[i] > 190){ // yellow/green
+                      r = 204
+                      g = 255
+                      b = 0
+                    } else if (dataArray[i] > 180){ // blue/green
+                      r = 0
+                      g = 219
+                      b = 131
+                    } else { // light blue
+                      r = 0
+                      g = 199
+                      b = 255
+                    }
+        
+                    canvasCtx.fillStyle = `rgb(${r},${g},${b})`;
+                    canvasCtx.fillRect(x, (canvas.height - barHeight), barWidth, barHeight);
+        
+                    x += barWidth + 10;
+                }
+            }
+
+            renderFrame();
         }
-
-
+        
+        
     }, [starter])
-
+    
+    
+   
 
     const start = () => setStarter(p => !p);
 
 
-
-
-
-
-
-
-
     return (
         <>
-            <audio src={song2} controls></audio>
+            <audio src={song} controls></audio>
             <button onClick={start}>turn effects on</button>
-            
+            <canvas id='canvas'></canvas>
         </>
     );
 }
