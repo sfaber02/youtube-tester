ff0ef72fb0a9da6bd97522940b859f3bdc5d18ba
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-05-12 01:13:27 -0400

    got fetch working from remote!

--- src/app.js
+++ src/app.js
@@ -21,134 +21,137 @@ const App = () => {
     if (starter) {
       
       const ctx = new (window.AudioContext || window.webkitAudioContext)();
-      // let audio;
-      // fetch("http://www.shawnfaber.com/audio/01%20-%20Glue%20of%20the%20World.flac", {mode: 'no-cors'})
-      //   .then(data => data.arrayBuffer())
-      //   .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
-      //   .then(decodedAudio => {
-      //     audio = decodedAudio;
-      //   })
-      //   .catch(err => console.log(err));
+      let audio;
+      fetch("http://www.shawnfaber.com/audio/01%20-%20Metronomic%20Underground%20-%20USEE19694710%20-%20355425551.flac")
+        .then(data => data.arrayBuffer())
+        .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+        .then(decodedAudio => {
+            audio = decodedAudio;
+            const playSound = ctx.createBufferSource();
+            playSound.buffer = audio;
+            playSound.connect(ctx.destination);
+            playSound.start(ctx.currentTime);
+        })
       
 
-      const audioElement = document.getElementById('audio1');
+    //   const audioElement = document.getElementById('audio1');
       
 
-      audioElement.crossOrigin = 'anonymous';
-      const mediaElement = ctx.createMediaElementSource(audioElement);
-      const canvas = document.getElementById('canvas');
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      const canvasCtx = canvas.getContext('2d');
+    //   audioElement.crossOrigin = 'anonymous';
+    //   const mediaElement = ctx.createMediaElementSource(audioElement);
+    //   const canvas = document.getElementById('canvas');
+    //   canvas.width = window.innerWidth;
+    //   canvas.height = window.innerHeight;
+    //   const canvasCtx = canvas.getContext('2d');
 
-      //EFFECT CHAIN 1
-      // const filter = ctx.createBiquadFilter();
-      // filter.frequency.value = 100;
+    //   //EFFECT CHAIN 1
+    //   // const filter = ctx.createBiquadFilter();
+    //   // filter.frequency.value = 100;
 
-      // const compressor = ctx.createDynamicsCompressor();
-      // compressor.ratio.value = 5;
-      // compressor.threshold.value = -50;
+    //   // const compressor = ctx.createDynamicsCompressor();
+    //   // compressor.ratio.value = 5;
+    //   // compressor.threshold.value = -50;
 
-      // const delay = ctx.createDelay();
-      // delay.delayTime.value = .5;
-
-
-      // mediaElement.connect(delay);
-      // delay.connect(filter);
-      // filter.connect(compressor);
-
-      // compressor.connect(ctx.destination);
+    //   // const delay = ctx.createDelay();
+    //   // delay.delayTime.value = .5;
 
 
-      // EFFECT CHAIN 2
-      const delayNode = ctx.createDelay(100)
-      const feedbackNode = ctx.createGain();
-      const bypassNode = ctx.createGain();
-      const masterNode = ctx.createGain();
-      const filter = ctx.createBiquadFilter();
-      const filter2 = ctx.createBiquadFilter();
-      const analyser = ctx.createAnalyser();
+    //   // mediaElement.connect(delay);
+    //   // delay.connect(filter);
+    //   // filter.connect(compressor);
 
-      // .02 .05 1  = slapback delay 
-      delayNode.delayTime.value = .02;
-      feedbackNode.gain.value = .05;
-      bypassNode.gain.value = 1;
-      filter.frequency.value = 20000;
-      filter2.frequency.value = 20000;
+    //   // compressor.connect(ctx.destination);
 
 
-      //wire up nodes
-      mediaElement.connect(analyser);
-      analyser.connect(delayNode);
-      delayNode.connect(feedbackNode);
-      feedbackNode.connect(delayNode);
+    //   // EFFECT CHAIN 2
+    //   const delayNode = ctx.createDelay(100)
+    //   const feedbackNode = ctx.createGain();
+    //   const bypassNode = ctx.createGain();
+    //   const masterNode = ctx.createGain();
+    //   const filter = ctx.createBiquadFilter();
+    //   const filter2 = ctx.createBiquadFilter();
+    //   const analyser = ctx.createAnalyser();
 
-      delayNode.connect(bypassNode);
-      bypassNode.connect(filter);
-      filter.connect(masterNode);
-      mediaElement.connect(filter2);
-      filter2.connect(masterNode);
+    //   // .02 .05 1  = slapback delay 
+    //   delayNode.delayTime.value = .02;
+    //   feedbackNode.gain.value = .05;
+    //   bypassNode.gain.value = 1;
+    //   filter.frequency.value = 20000;
+    //   filter2.frequency.value = 20000;
 
-      masterNode.connect(ctx.destination);
 
-      mediaElement.connect(ctx.destination);
+    //   //wire up nodes
+    //   mediaElement.connect(analyser);
+    //   analyser.connect(delayNode);
+    //   delayNode.connect(feedbackNode);
+    //   feedbackNode.connect(delayNode);
 
-      //set up analyser
-      console.log(canvas.height, canvas.width);
-      analyser.fftSize = 16384;
-      let bufferLength = analyser.frequencyBinCount;
-      let dataArray = new Uint8Array(bufferLength);
-      const barWidth = (canvas.width / bufferLength) * 50;
-      let barHeight;
-      let x = 0;
+    //   delayNode.connect(bypassNode);
+    //   bypassNode.connect(filter);
+    //   filter.connect(masterNode);
+    //   mediaElement.connect(filter2);
+    //   filter2.connect(masterNode);
 
-      // render visualizer
-      const renderFrame = () => {
-        requestAnimationFrame(renderFrame);
-        x = 0;
-        // console.log (analyser);
+    //   masterNode.connect(ctx.destination);
 
-        analyser.getByteFrequencyData(dataArray);
+    //   mediaElement.connect(ctx.destination);
 
-        canvasCtx.fillStyle = "rgba(0,0,0,0.2)"; // Clears canvas before rendering bars (black with opacity 0.2)
-        canvasCtx.fillRect(0, 0, canvas.width, canvas.height); // Fade effect, set opacity to 1 for sharper rendering of bars
+    //   //set up analyser
+    //   console.log(canvas.height, canvas.width);
+    //   analyser.fftSize = 16384;
+    //   let bufferLength = analyser.frequencyBinCount;
+    //   let dataArray = new Uint8Array(bufferLength);
+    //   const barWidth = (canvas.width / bufferLength) * 50;
+    //   let barHeight;
+    //   let x = 0;
 
-        let r, g, b;
-        let bars = 150;
+    //   // render visualizer
+    //   const renderFrame = () => {
+    //     requestAnimationFrame(renderFrame);
+    //     x = 0;
+    //     // console.log (analyser);
 
-        for (let i = 0; i < bars; i++) {
-          barHeight = (dataArray[i] * 2);
-          // console.log (dataArray[i]);
-          if (dataArray[i] > 210) { // pink
-            r = 250
-            g = 0
-            b = 255
-          } else if (dataArray[i] > 200) { // yellow
-            r = 250
-            g = 255
-            b = 0
-          } else if (dataArray[i] > 190) { // yellow/green
-            r = 204
-            g = 255
-            b = 0
-          } else if (dataArray[i] > 180) { // blue/green
-            r = 0
-            g = 219
-            b = 131
-          } else { // light blue
-            r = 0
-            g = 199
-            b = 255
-          }
+    //     analyser.getByteFrequencyData(dataArray);
 
-          canvasCtx.fillStyle = `rgb(${r},${g},${b})`;
-          canvasCtx.fillRect(x, (canvas.height - barHeight), barWidth, barHeight);
+    //     canvasCtx.fillStyle = "rgba(0,0,0,0.2)"; // Clears canvas before rendering bars (black with opacity 0.2)
+    //     canvasCtx.fillRect(0, 0, canvas.width, canvas.height); // Fade effect, set opacity to 1 for sharper rendering of bars
 
-          x += barWidth + 10;
-        }
-      }
+    //     let r, g, b;
+    //     let bars = 150;
 
-      renderFrame();
+    //     for (let i = 0; i < bars; i++) {
+    //       barHeight = (dataArray[i] * 2);
+    //       // console.log (dataArray[i]);
+    //       if (dataArray[i] > 210) { // pink
+    //         r = 250
+    //         g = 0
+    //         b = 255
+    //       } else if (dataArray[i] > 200) { // yellow
+    //         r = 250
+    //         g = 255
+    //         b = 0
+    //       } else if (dataArray[i] > 190) { // yellow/green
+    //         r = 204
+    //         g = 255
+    //         b = 0
+    //       } else if (dataArray[i] > 180) { // blue/green
+    //         r = 0
+    //         g = 219
+    //         b = 131
+    //       } else { // light blue
+    //         r = 0
+    //         g = 199
+    //         b = 255
+    //       }
+
+    //       canvasCtx.fillStyle = `rgb(${r},${g},${b})`;
+    //       canvasCtx.fillRect(x, (canvas.height - barHeight), barWidth, barHeight);
+
+    //       x += barWidth + 10;
+    //     }
+    //   }
+
+    //   renderFrame();
     }
 
 
@@ -162,8 +165,8 @@ const App = () => {
 
   return (
     <>
-      <audio src="http://shawnfaber.latuli.pe/audio/Kielbasa.mp3" controls id='audio1' ></audio>
-      <button onClick={start}>turn effects on</button>
+      {/* <audio src="http://www.shawnfaber.com/audio/01%20-%20Loved.mp3" controls id='audio1' ></audio> */}
+      <button onClick={start}>play!</button>
       <canvas id='canvas'></canvas>
     </>
   );
